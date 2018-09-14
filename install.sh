f35796f5afce9a9e95bcdc26da3df511bfcec003
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-09-14 11:52:04 -0600

    added requirements.txt

--- install.sh
+++ install.sh
@@ -67,6 +67,7 @@ sudo apt-get install chromium-browser -y
 ### Install Python3
 
 sudo apt-get install python3 python3-pip -y
+sudo chown -R "$USER":"$USER" /opt/Port-Crawler-Py
 sudo pip3 install -r /opt/Port-Crawler-Py/requirements.txt
 
 
