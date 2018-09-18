864e2a1767af9bc05ae9091311417b5a2828904f
Spencer Heywood <l.spencer.heywood@gmail.com>
2018-09-17 20:25:35 -0600

    separated Debian specific commands from the rest

--- install.sh
+++ install.sh
@@ -11,34 +11,32 @@ PWD="$(pwd)"
 sudo mkdir -p /opt/
 sudo chown -R "$USER:$USER" /opt
 git clone https://github.com/heywoodlh/Port-Crawler-Py /opt/Port-Crawler-Py
-cd Port-Crawler-Py
+
+if [[ -f /etc/debian_version ]]
+then
+	DEBIAN='True'
+fi
 
 
+if [[ "$DEBIAN" == 'True' ]]
+then	
+	
+	wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+	echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-6.x.list
+	
+	sudo apt-get update
+	sudo apt-get install python3 python3-pip git gcc make clang libpcap-dev openjdk-8-jdk kibana elasticsearch -y
 
-### Install Java
-
-sudo apt-get update
-sudo apt-get install openjdk-8-jdk -y
+fi
 
 
-### Elastic repository install
-
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
-echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-6.x.list
-
-### Elasticsearch install
-
-sudo apt-get update
-sudo apt-get install elasticsearch -y
 sudo sed -i 's/#cluster.name: my-application/cluster.name: port-crawler/g' /etc/elasticsearch/elasticsearch.yml
 sudo sed -i 's/#network.host: 192.168.0.1/network.host: 127.0.0.1/g' /etc/elasticsearch/elasticsearch.yml
 sudo systemctl enable elasticsearch.service
 sudo systemctl start elasticsearch.service
 
 
-### Kibana install
 
-sudo apt-get install kibana -y
 sudo sed -i 's/#server.port: 5601/server.port: 5601/g' /etc/kibana/kibana.yml
 sudo sed -i 's/#server.host: "localhost"/server.host: '"$IP_OR_HOSTNAME"'/g' /etc/kibana/kibana.yml
 sudo systemctl enable kibana.service
@@ -46,9 +44,7 @@ sudo systemctl restart kibana.service
 
 
 
-### Masscan install
 
-sudo apt-get install git gcc make clang libpcap-dev -y
 sudo chown -R "$USER:$USER" /opt
 cd /opt
 git clone https://github.com/robertdavidgraham/masscan
@@ -57,9 +53,7 @@ make
 sudo cp bin/masscan /usr/bin/
 
 
-### Install Python3
 
-sudo apt-get install python3 python3-pip -y
 sudo chown -R "$USER":"$USER" /opt/Port-Crawler-Py
 sudo pip3 install -r /opt/Port-Crawler-Py/requirements.txt
 
